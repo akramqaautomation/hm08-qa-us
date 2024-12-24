24928e0133207e0e91d64ae11c24f8dfd2125ff1
akramqaautomation <abubakerakram404@yahoo.com>
2024-12-24 00:27:06 -0500

    add corrections

--- page.js
+++ page.js
@@ -1,28 +1,115 @@
-{
-    "name": "hm08-qa-us",
-    "version": "1.0.0",
-    "description": "",
-    "main": "index.js",
-    "scripts": {
-      "lint": "npx eslint .",
-      "build": "rm -rf ./node_modules && npm install",
-      "wdio": "wdio run ./wdio.conf.js"
-    },
-    "keywords": [],
-    "author": "",
-    "license": "ISC",
-    "devDependencies": {
-      "@wdio/cli": "^8.6.7",
-      "@wdio/devtools-service": "^8.6.9",
-      "@wdio/local-runner": "^8.6.7",
-      "@wdio/mocha-framework": "^8.6.6",
-      "@wdio/spec-reporter": "^8.6.6",
-      "eslint": "^8.38.0",
-      "eslint-plugin-mocha": "^10.1.0",
-      "eslint-plugin-wdio": "^8.0.14",
-      "geckodriver": "^3.2.0",
-      "wdio-eslinter-service": "^0.0.4",
-      "wdio-geckodriver-service": "^4.1.1",
-      "wdio-intercept-service": "4.0.0"
-    }
-  }  
+module.exports = {
+
+  // Inputs
+  fromField: '#from',
+  toField: '#to',
+  phoneNumberField: '#phone',
+  codeField: '#code',
+  cardNumberInput: '.card-number-input #number',
+  cardCode: '.card-code-input #code',
+  messageInput: '#comment',
+
+  // Buttons
+  callATaxiButton: 'button=Call a taxi',
+  phoneNumberButton: '//div[starts-with(text(), "Phone number")]',
+  nextButton: 'button=Next',
+  confirmButton: 'button=Confirm',
+  paymentButton: '.pp-button',
+  supportiveButton: 'div=Supportive',
+  addCardButton: '.pp-row.disabled',
+  linkButton: '//button[contains(text(), "Link")]',
+  closeCardModalButton: '.payment-picker .close-button',
+  iceCreamPlusButton: '.counter-plus',
+  orderButton: '.smart-button',
+
+  // Checkboxes
+  blanketsSwitch: '.r-sw',
+
+  // Counters
+  iceCreamCounter: '.counter-value',
+
+  // Modals
+  phoneNumberModal: '.modal',
+  paymentModal: '.payment-picker .modal .section',
+  orderModal: '.order-body',
+
+  // Functions
+  fillAddresses: async function (from, to) {
+      const fromField = await $(this.fromField);
+      await fromField.setValue(from);
+      const toField = await $(this.toField);
+      await toField.setValue(to);
+      const callATaxiButton = await $(this.callATaxiButton);
+      await callATaxiButton.waitForDisplayed();
+      await callATaxiButton.click();
+  },
+  fillPhoneNumber: async function (phoneNumber) {
+      const phoneNumberButton = await $(this.phoneNumberButton);
+      await phoneNumberButton.waitForDisplayed();
+      await phoneNumberButton.click();
+      const phoneNumberModal = await $(this.phoneNumberModal);
+      await phoneNumberModal.waitForDisplayed()
+      const phoneNumberField = await $(this.phoneNumberField);
+      await phoneNumberField.waitForDisplayed();
+      await phoneNumberField.setValue(phoneNumber);
+  },
+  submitPhoneNumber: async function (phoneNumber) {
+      await this.fillPhoneNumber(phoneNumber);
+      // we are starting interception of request from the moment of method call
+      await browser.setupInterceptor();
+      await $(this.nextButton).click();
+      // we should wait for response
+      // eslint-disable-next-line wdio/no-pause
+      await browser.pause(2000);
+      const codeField = await $(this.codeField);
+      // collect all responses
+      const requests = await browser.getRequests();
+      // use first response
+      await expect(requests.length).toBe(1)
+      const code = await requests[0].response.body.code
+      await codeField.setValue(code)
+      await $(this.confirmButton).click()
+  },
+  clickSupportiveButton: async function () {
+      const supportiveButton = await $(this.supportiveButton);
+      await supportiveButton.click()
+  },
+  clickPaymentButton: async function () {
+      const paymentButton = await $(this.paymentButton);
+      await paymentButton.click();
+  },
+  fillCardDetails: async function (cardNumber, cvvNumber, linkAndClose = false) {
+      const addCardButton = await $(this.addCardButton);
+      await addCardButton.click();
+      await $(this.cardNumberInput).setValue(cardNumber);
+      await $(this.cardCode).setValue(cvvNumber);
+      await browser.keys("\ue004");
+      if (linkAndClose) {
+          await ($(this.linkButton)).click();
+          await $(this.closeCardModalButton).click(); // close payment window
+      }
+  },
+  writeMessage: async function (message = 'Sample Message') {
+      const messageInputBox = $(this.messageInput);
+      await messageInputBox.setValue(message);
+  },
+  clickBlanketSwitch: async function () {
+      const blanketsSwitch = await $(this.blanketsSwitch);
+      await blanketsSwitch.waitForDisplayed();
+      await blanketsSwitch.scrollIntoView();
+      await blanketsSwitch.click();
+  },
+  addTwoIceCreams: async function () {
+      const iceCreamPlusButton = await $(this.iceCreamPlusButton);
+      await iceCreamPlusButton.waitForDisplayed();
+      await iceCreamPlusButton.scrollIntoView();
+      await iceCreamPlusButton.click();
+      await iceCreamPlusButton.click();
+  },
+  clickOrderButton: async function () {
+      const orderButton = await $(this.orderButton);
+      await orderButton.waitForDisplayed();
+      await orderButton.click();
+  }
+
+};
